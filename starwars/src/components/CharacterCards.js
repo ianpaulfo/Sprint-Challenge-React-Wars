66a2d3b041d1743f3a65234edae954edf5384bc7
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-02-09 20:22:43 -0700

    added styling via Styled components / MVP

--- starwars/src/components/CharacterCards.js
+++ starwars/src/components/CharacterCards.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Card } from "./CharacterStyles";
 
 const CharacterCards = (props) => (
 
         <div>
           {props.characters.map(character => {
             return (
-              <div key={character.url}>
+              <Card key={character.url}>
                 <h1>{character.name}</h1>
                 <p> Height: {character.height}</p>
                 <p> Hair-Color: {character.hair_color}</p>
                 <p> DOB: {character.birth_year}</p>
-              </div>
+              </Card>
             );
           })}
         </div>
---
+++ starwars/src/components/CharacterStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  text-align: center;
+  background-color: grey;
+  color: black;
+  border: 2.5px solid whitesmoke;
+  padding: 20px;
+  width: 25%;
+  border-radius: 20px;
+  margin: 1% auto;
+  opacity: 0.85;
+`;
